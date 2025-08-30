b6ceb13788c3dd1af6381941a4032db6f2e890ec
Ygor Castor <ygorcastor@gmail.com>
2025-08-30 08:37:07 +0200

    chore: Added Environment Variables

--- config/account_server/network.exs
+++ config/account_server/network.exs
@@ -4,4 +4,4 @@ alias Hush.Provider.SystemEnvironment
 
 config :account_server, :network,
   bind_ip: {:hush, SystemEnvironment, "ACCOUNT_BIND_IP", default: "0.0.0.0"},
-  port: {:hush, SystemEnvironment, "ACCOUNT_PORT", default: 6901, cast: :integer}
+  port: {:hush, SystemEnvironment, "ACCOUNT_PORT", default: 6900, cast: :integer}
--- config/zone_server/network.exs
+++ config/zone_server/network.exs
@@ -4,5 +4,4 @@ alias Hush.Provider.SystemEnvironment
 
 config :zone_server, :network,
   bind_ip: {:hush, SystemEnvironment, "ZONE_BIND_IP", default: "0.0.0.0"},
-  broadcast_addr: {:hush, SystemEnvironment, "ZONE_BROADCAST_ADDR", default: "127.0.0.1"},
   port: {:hush, SystemEnvironment, "ZONE_PORT", default: 5121, cast: :integer}
