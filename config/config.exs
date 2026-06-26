f098c6a559a8a925e17e84891952ee7abecb917e
Cλstor <ygorcastor@gmail.com>
2026-06-26 10:26:21 +0200

    chore: Dropped unused Packetver module

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,7 @@ import Config
 
 config :commons,
   env: config_env(),
-  ecto_repos: [Aesir.Repo],
-  packetver: "20211103"
+  ecto_repos: [Aesir.Repo]
 
 import_config "database.exs"
 import_config "libcluster.exs"
