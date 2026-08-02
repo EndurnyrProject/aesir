61f8d2fdd210e35e2a8513352a431386c7b285a9
Cλstor <ygorcastor@gmail.com>
2026-08-02 13:55:03 +0200

    feat(zone): add loot ownership config values

--- apps/zone_server/lib/aesir/zone_server/config.ex
+++ apps/zone_server/lib/aesir/zone_server/config.ex
@@ -13,6 +13,13 @@ defmodule Aesir.ZoneServer.Config do
   @default_party_even_share_bonus 0
   @default_exp_bonus_attacker 25
   @default_exp_bonus_max_attacker 12
+  @default_item_first_get_time 3000
+  @default_item_second_get_time 2000
+  @default_item_third_get_time 2000
+  @default_mvp_item_first_get_time 10_000
+  @default_mvp_item_second_get_time 10_000
+  @default_mvp_item_third_get_time 2000
+  @default_first_attack_loot_bonus 30
   @default_death_penalty_base 1
   @default_death_penalty_job 1
   @default_natural_break_rate 0
@@ -80,6 +87,61 @@ defmodule Aesir.ZoneServer.Config do
         :zone_server,
         :exp_bonus_max_attacker,
         @default_exp_bonus_max_attacker
+      )
+
+  @doc "First loot-owner exclusive pickup window for dropped items, in milliseconds."
+  @spec item_first_get_time() :: pos_integer()
+  def item_first_get_time,
+    do: Application.get_env(:zone_server, :item_first_get_time, @default_item_first_get_time)
+
+  @doc "Additional pickup window for the second loot owner, in milliseconds."
+  @spec item_second_get_time() :: pos_integer()
+  def item_second_get_time,
+    do: Application.get_env(:zone_server, :item_second_get_time, @default_item_second_get_time)
+
+  @doc "Additional pickup window for the third loot owner, in milliseconds."
+  @spec item_third_get_time() :: pos_integer()
+  def item_third_get_time,
+    do: Application.get_env(:zone_server, :item_third_get_time, @default_item_third_get_time)
+
+  @doc "First loot-owner exclusive pickup window for MVP drops, in milliseconds."
+  @spec mvp_item_first_get_time() :: pos_integer()
+  def mvp_item_first_get_time,
+    do:
+      Application.get_env(
+        :zone_server,
+        :mvp_item_first_get_time,
+        @default_mvp_item_first_get_time
+      )
+
+  @doc "Additional pickup window for the second MVP loot owner, in milliseconds."
+  @spec mvp_item_second_get_time() :: pos_integer()
+  def mvp_item_second_get_time,
+    do:
+      Application.get_env(
+        :zone_server,
+        :mvp_item_second_get_time,
+        @default_mvp_item_second_get_time
+      )
+
+  @doc "Additional pickup window for the third MVP loot owner, in milliseconds."
+  @spec mvp_item_third_get_time() :: pos_integer()
+  def mvp_item_third_get_time,
+    do:
+      Application.get_env(
+        :zone_server,
+        :mvp_item_third_get_time,
+        @default_mvp_item_third_get_time
+      )
+
+  @doc "Bonus percentage of total damage credited to a mob's first loot attacker."
+  @spec first_attack_loot_bonus() :: non_neg_integer()
+  def first_attack_loot_bonus,
+    do:
+      Application.get_env(
+        :zone_server,
+        :first_attack_loot_bonus,
+        @default_first_attack_loot_bonus
       )
 
   @doc """
